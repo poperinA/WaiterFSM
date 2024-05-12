070ed55d3deb8746d3041ed0a1a3eca8634c411b
poperinA <falishaakhan05@gmail.com>
2024-05-13 04:44:41 +0800

    testing each state script

--- Assets/Scripts/WaiterFSM.cs
+++ Assets/Scripts/WaiterFSM.cs
@@ -1,8 +1,47 @@
 using UnityEngine;
 
-public class WaiterFSM : MonoBehaviour
+public class WaiterFSM : WaiterStateFSM
 {
     protected WaiterStateFSM currentState;
+
+    public WaiterFSM() : base(null) { } // Add constructor for WaiterFSM
+
+
+    void Start()
+    {
+        //initialising states
+
+        //idle state
+        IdleState idleState = new IdleState(this);
+
+        //table assignment state
+        TableAssignmentState tableAssignmentState = new TableAssignmentState(this);
+
+        //customer servicing states
+        CustomerServiceState customerServiceState = new CustomerServiceState(this);
+        RefillState refillServiceState = new RefillState(this);
+
+        //customer table service states
+        ClearTableState clearTableState = new ClearTableState(this);
+        ServingState servingState = new ServingState(this);
+        PerformanceState performanceState = new PerformanceState(this);
+
+        //order taking states
+        OrderTakingState orderTakingState = new OrderTakingState(this);
+        EnquiryState enquiryTakingState = new EnquiryState(this);
+        RecommendationState recommendationTakingState = new RecommendationState(this);
+        ApologyState apologyState = new ApologyState(this);
+
+        // Initialize the current state to IdleState
+        TransitionToState(idleState);
+
+    }
+
+    void Update()
+    {
+        if (currentState != null)
+            currentState.Execute();
+    }
 
     public void TransitionToState(WaiterStateFSM nextState)
     {
@@ -13,9 +52,4 @@ public class WaiterFSM : MonoBehaviour
         currentState.Enter();
     }
 
-    void Update()
-    {
-        if (currentState != null)
-            currentState.Execute();
-    }
 }
